85a2cb23197ece1925c4dcaa5bbf2d95b1ac3e99
Vamsi Ravula <codingsaduvu@gmail.com>
2025-04-08 15:25:59 -0700

    Add icons and content type

--- catalog-data.js
+++ catalog-data.js
@@ -4,6 +4,7 @@ const catalogData = [
         id: 1,
         title: "Kaoto - The Integration Designer for Red Hat build of Apache Camel - Kaoto",
         category: "Integration",
+        contentType: "arcade",
         imageUrl: "images/camel_rb.png",
         link: "https://interact.redhat.com/share/lDqpjE28hCKNhqIwEMUT"
     },
@@ -11,6 +12,7 @@ const catalogData = [
         id: 2,
         title: "Event Streaming and Real Time Data Processing with Apache Kafka",
         category: "Messaging & Data Streaming",
+        contentType: "arcade",
         imageUrl: "images/kafka_rb.png",
         link: "https://interact.redhat.com/share/pYiQKLDlRICTtqM3G8Mw"
     },
@@ -18,6 +20,7 @@ const catalogData = [
         id: 3,
         title: "Java Modernization Paths",
         category: "Runtimes",
+        contentType: "slides",
         imageUrl: "images/runtimes_rb.png",
         link: "https://docs.google.com/presentation/d/18WhskOBk9goOlSpR0yNHlFMYkmdexmnB-HJrK7ObDlM/edit#slide=id.g2d2206166c3_0_2129"
     },
@@ -25,6 +28,7 @@ const catalogData = [
         id: 4,
         title: "EAP 7 to 8 Migration",
         category: "Runtimes",
+        contentType: "slides",
         imageUrl: "images/runtimes_rb.png",
         link: "https://drive.google.com/file/d/1P600hWUZrRi4yFd9Zw1is5Yk_NUnNEtE/view"
     },
@@ -32,6 +36,7 @@ const catalogData = [
         id: 5,
         title: "EAP on OpenShift",
         category: "Runtimes",
+        contentType: "video",
         imageUrl: "images/runtimes_rb.png",
         link: "https://drive.google.com/file/d/1P600hWUZrRi4yFd9Zw1is5Yk_NUnNEtE/view"
     },
@@ -39,6 +44,7 @@ const catalogData = [
         id: 6,
         title: "Red Hat Connectivity Link",
         category: "Application Connectivity",
+        contentType: "webpage",
         imageUrl: "images/RHCL_rb.png",
         link: "https://www.redhat.com/en/technologies/cloud-computing/connectivity-link"
     },
@@ -46,6 +52,7 @@ const catalogData = [
         id: 7,
         title: "Extending Red Hat Connectivity Link to non Kubernetes environments with Red Hat Service Interconnect",
         category: "Application Connectivity",
+        contentType: "video",
         imageUrl: "images/rhsi_rb.png",
         link: "https://drive.google.com/file/d/1tVKPx8po55A0Al4BjZtQROo_DmqFveTE/view?usp=drive_link"
     },
@@ -53,6 +60,7 @@ const catalogData = [
         id: 8,
         title: "Quarkus Developer Productivity Features",
         category: "Runtimes",
+        contentType: "video",
         imageUrl: "images/runtimes_rb.png",
         link: "https://www.youtube.com/playlist?list=PLoQ0Ba0OL3iuwy2Bw3AkJbErcVOyufMF4"
     },
@@ -60,6 +68,7 @@ const catalogData = [
         id: 9,
         title: "Creating Your First Quarkus App",
         category: "Runtimes",
+        contentType: "video",
         imageUrl: "images/runtimes_rb.png",
         link: "https://youtu.be/WXzvg94Jy0s?si=1ohJB-0yFLNJlzgo"
     },
@@ -67,6 +76,7 @@ const catalogData = [
         id: 10,
         title: "AI for Java Application Developers (Quarkus & LangChain4j)",
         category: "Runtimes",
+        contentType: "video",
         imageUrl: "images/runtimes_rb.png",
         link: "https://youtu.be/WXzvg94Jy0s?si=1ohJB-0yFLNJlzgo"
     },
@@ -74,6 +84,7 @@ const catalogData = [
         id: 11,
         title: "Connect LLMs to the world with Red Hat build of Apache Camel",
         category: "Integration",
+        contentType: "arcade",
         imageUrl: "images/camel_rb.png",
         link: "https://interact.redhat.com/share/ahGKXu7HfZ3HcbXXM3SQ"
     }
