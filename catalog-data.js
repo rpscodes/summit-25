a510ba954969688a6b06678c644b26b8c6364f37
Vamsi Ravula <codingsaduvu@gmail.com>
2025-04-08 15:05:01 -0700

    Seperate data and styling

---
+++ catalog-data.js
@@ -0,0 +1,80 @@
+// Catalog items data
+const catalogData = [
+    {
+        id: 1,
+        title: "Kaoto - The Integration Designer for Red Hat build of Apache Camel - Kaoto",
+        category: "Integration",
+        imageUrl: "images/camel_rb.png",
+        link: "https://interact.redhat.com/share/lDqpjE28hCKNhqIwEMUT"
+    },
+    {
+        id: 2,
+        title: "Event Streaming and Real Time Data Processing with Apache Kafka",
+        category: "Messaging & Data Streaming",
+        imageUrl: "images/kafka_rb.png",
+        link: "https://interact.redhat.com/share/pYiQKLDlRICTtqM3G8Mw"
+    },
+    {
+        id: 3,
+        title: "Java Modernization Paths",
+        category: "Runtimes",
+        imageUrl: "images/runtimes_rb.png",
+        link: "https://docs.google.com/presentation/d/18WhskOBk9goOlSpR0yNHlFMYkmdexmnB-HJrK7ObDlM/edit#slide=id.g2d2206166c3_0_2129"
+    },
+    {
+        id: 4,
+        title: "EAP 7 to 8 Migration",
+        category: "Runtimes",
+        imageUrl: "images/runtimes_rb.png",
+        link: "https://drive.google.com/file/d/1P600hWUZrRi4yFd9Zw1is5Yk_NUnNEtE/view"
+    },
+    {
+        id: 5,
+        title: "EAP on OpenShift",
+        category: "Runtimes",
+        imageUrl: "images/runtimes_rb.png",
+        link: "https://drive.google.com/file/d/1P600hWUZrRi4yFd9Zw1is5Yk_NUnNEtE/view"
+    },
+    {
+        id: 6,
+        title: "Red Hat Connectivity Link",
+        category: "Application Connectivity",
+        imageUrl: "images/RHCL_rb.png",
+        link: "https://www.redhat.com/en/technologies/cloud-computing/connectivity-link"
+    },
+    {
+        id: 7,
+        title: "Extending Red Hat Connectivity Link to non Kubernetes environments with Red Hat Service Interconnect",
+        category: "Application Connectivity",
+        imageUrl: "images/rhsi_rb.png",
+        link: "https://drive.google.com/file/d/1tVKPx8po55A0Al4BjZtQROo_DmqFveTE/view?usp=drive_link"
+    },
+    {
+        id: 8,
+        title: "Quarkus Developer Productivity Features",
+        category: "Runtimes",
+        imageUrl: "images/runtimes_rb.png",
+        link: "https://www.youtube.com/playlist?list=PLoQ0Ba0OL3iuwy2Bw3AkJbErcVOyufMF4"
+    },
+    {
+        id: 9,
+        title: "Creating Your First Quarkus App",
+        category: "Runtimes",
+        imageUrl: "images/runtimes_rb.png",
+        link: "https://youtu.be/WXzvg94Jy0s?si=1ohJB-0yFLNJlzgo"
+    },
+    {
+        id: 10,
+        title: "AI for Java Application Developers (Quarkus & LangChain4j)",
+        category: "Runtimes",
+        imageUrl: "images/runtimes_rb.png",
+        link: "https://youtu.be/WXzvg94Jy0s?si=1ohJB-0yFLNJlzgo"
+    },
+    {
+        id: 11,
+        title: "Connect LLMs to the world with Red Hat build of Apache Camel",
+        category: "Integration",
+        imageUrl: "images/camel_rb.png",
+        link: "https://interact.redhat.com/share/ahGKXu7HfZ3HcbXXM3SQ"
+    }
+]; 
